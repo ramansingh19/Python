c08a3a30e29cfe04ae142006e1f90cc6e5c6712e
ramansingh19 <raman.01kumar1@gmail.com>
2026-02-27 14:41:49 +0530

    Comment out Student class and related print statements

--- Basics/practice.py
+++ Basics/practice.py
@@ -1,12 +1,17 @@
-class Student:
-    def __str__(self):
-        return "Raman"
+# class Student:
+#     def __str__(self):
+#         return "Raman"
 
-    def __repr__(self):
-        return "Aman"
+#     def __repr__(self):
+#         return "Aman"
 
-s = Student()
+# s = Student()
 
-print(str(s))  
-print(repr(s))  
-print(s)        
+# print(str(s))  
+# print(repr(s))  
+# print(s)        
+
+print(11 / 3)
+print(11 // 3)
+print(10 % 3)
+print(-11 // 3)
