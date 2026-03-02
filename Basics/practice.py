0d66ea8f9b903dd869105d70850fce65580c8de5
ramansingh19 <raman.01kumar1@gmail.com>
2026-03-02 11:59:37 +0530

    Refactor practice.py to uncomment code examples and add tempCodeRunnerFile.py for testing

--- Basics/practice.py
+++ Basics/practice.py
@@ -7,11 +7,26 @@
 
 # s = Student()
 
-# print(str(s))  
-# print(repr(s))  
-# print(s)        
+# print(str(s))
+# print(repr(s))
+# print(s)
 
-print(11 / 3)
-print(11 // 3)
-print(10 % 3)
-print(-11 // 3)
+# print(11 / 3)
+# print(11 // 3)
+# print(10 % 3)
+# print(-11 // 3)
+
+# text = "Python"
+# print(text[-1])
+
+# text = "Hello"
+# print(text * 3)
+
+# text = "Python"
+# print(text[10])
+
+text = "abc"
+print(id(text))
+text = text + "d"
+print(id(text))
+
