38f9d7377dd9f81e203adf097a1dbf8ab338240b
ramansingh19 <raman.01kumar1@gmail.com>
2026-03-25 23:44:56 +0530

    ss

--- OOPs/01_solution.py
+++ OOPs/01_solution.py
@@ -1,12 +1,34 @@
-class Car():
+#Class & Object
+class Car:
   def __init__(self ,userbrand , usermodel):
     self.userbrand = userbrand
     self.usermodel = usermodel
 
+  #Encapsulation --> when we want privatisation we use __ this symbol so we when we want to access brand we have to call get_brand first ,
+  def get_brand(self):
+    return self.__userbrand + "!"
 
-my_car = Car("BMW" , "M4")
-print(my_car.userbrand)
-print(my_car.usermodel)
+  def fullname(self):
+    return f"{self.userbrand} , {self.usermodel}"
+
+#inheritance
+class ElectricCar(Car):
+  def __init__(self , battery_size , userbrand , usermodel):
+    super().__init__(userbrand , usermodel)
+    self.battery_size = battery_size
+
+my_electricCar = ElectricCar("Tesla" , "S" , "500")
+
+# print(my_electricCar.userbrand)
+print(my_electricCar.get_brand())
+# print(my_electricCar.fullname())
+
+# my_car = Car("BMW" , "M4")
+# print(my_car.userbrand)
+# print(my_car.usermodel)
+# print(my_car.fullname())
+
+
 
 
 # self is use for communication btw these objects just like this in JS.
@@ -22,3 +44,5 @@ print(my_car.usermodel)
 # my_car = Car("BMW" , "M4")
 # print(my_car.userbrand)
 # print(my_car.usermodel)
+
+
---
+++ OOPs/02_solution.py
@@ -0,0 +1,9 @@
+class Cars():
+  def __init__(self , brand , model):
+    self.brand = brand
+    self.model = model
+    
+
+my_car = Cars("BMW" , "M4")
+print(my_car.brand)
+print(my_car.model)
